f258dc7293f15d520c803055599c9234c4bd0514
qianbin <cola.tin.com@gmail.com>
2020-03-05 22:58:26 +0800

    ecvrf: expose Config struct
    to support customized algo

---
+++ core.go
@@ -0,0 +1,232 @@
+package ecvrf
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"crypto/hmac"
+	"errors"
+	"math/big"
+)
+
+type core struct {
+	*Config
+	elliptic.Curve
+}
+
+func (c *core) Q() *big.Int {
+	return c.Params().N
+}
+
+func (c *core) OneN() int {
+	plen := c.Params().P.BitLen()
+	return (plen + 1) / 2
+}
+
+// Marshal marshals a point into compressed form specified in section 4.3.6 of ANSI X9.62.
+// It's the alias of `point_to_string` specified in VRF-draft-06 (https://tools.ietf.org/id/draft-irtf-cfrg-vrf-06.html#rfc.section.5.5).
+func (c *core) Marshal(x, y *big.Int) []byte {
+	byteLen := (c.Params().BitSize + 7) / 8
+	out := make([]byte, byteLen+1)
+
+	// compress format, 3 for odd y
+	out[0] = 2 + byte(y.Bit(0))
+
+	bytes := x.Bytes()
+
+	if n := len(bytes); byteLen > n {
+		copy(out[1+byteLen-n:], bytes)
+	} else {
+		copy(out[1:], bytes)
+	}
+	return out
+}
+
+// Unmarshal unmarshals a compressed point in the form specified in section 4.3.6 of ANSI X9.62.
+// It's the alias of `string_to_point` specified in VRF-draft-06 (https://tools.ietf.org/id/draft-irtf-cfrg-vrf-06.html#rfc.section.5.5).
+// This is borrowed from the project https://github.com/google/keytransparency.
+func (c *core) Unmarshal(in []byte) (x, y *big.Int, err error) {
+	byteLen := (c.Params().BitSize + 7) / 8
+	if (in[0] &^ 1) != 2 {
+		return nil, nil, errors.New("unrecognized point encoding")
+	}
+	if len(in) != 1+byteLen {
+		return nil, nil, errors.New("invalid point data length")
+	}
+	// Based on Routine 2.2.4 in NIST Mathematical routines paper
+	p := c.Params().P
+	tx := new(big.Int).SetBytes(in[1 : 1+byteLen])
+	y2 := c.Y2(c.Curve, tx)
+
+	ty := c.Sqrt(c.Curve, y2)
+	if ty == nil {
+		return nil, nil, errors.New("invalid point: y^2 is not a squire")
+	}
+
+	var y2c big.Int
+	y2c.Mul(ty, ty).Mod(&y2c, p)
+	if y2c.Cmp(y2) != 0 {
+		return nil, nil, errors.New("invalid point: sqrt(y2)^2 != y2")
+	}
+
+	if ty.Bit(0) != uint(in[0]&1) {
+		ty.Sub(p, ty)
+	}
+
+	// valid point: return it
+	return tx, ty, nil
+}
+
+func (c *core) Hash(data ...[]byte) []byte {
+	h := c.Hasher()
+	for _, e := range data {
+		h.Write(e)
+	}
+	return h.Sum(nil)
+}
+
+func (c *core) Mac(k []byte, m ...[]byte) []byte {
+	h := hmac.New(c.Hasher, k)
+	for _, e := range m {
+		h.Write(e)
+	}
+	return h.Sum(nil)
+}
+
+// HashToCurveTryAndIncrement https://tools.ietf.org/id/draft-irtf-cfrg-vrf-06.html#rfc.section.5.4.1.1
+func (c *core) HashToCurveTryAndIncrement(pkX, pkY *big.Int, alpha []byte) (hx, hy *big.Int, err error) {
+	pkBytes := c.Marshal(pkX, pkY)
+
+	for ctr := 0; ctr < 256; ctr++ {
+		hash := c.Hash(
+			[]byte{c.SuiteString, 0x01},
+			pkBytes,
+			alpha,
+			[]byte{byte(ctr)},
+		)
+
+		if hx, hy, err = c.Unmarshal(append([]byte{2}, hash...)); err == nil {
+			break
+		}
+	}
+
+	if hx == nil {
+		return nil, nil, errors.New("no valid point found")
+	}
+	hx, hy = c.ScalarMult(hx, hy, []byte{c.Cofactor})
+	return
+}
+
+func (c *core) GenerateNonce(sk *big.Int, hash []byte) *big.Int {
+	var (
+		q     = c.Q()
+		qlen  = q.BitLen()
+		rolen = (qlen + 7) / 8
+		holen = len(hash)
+		bh    = bits2octets(hash, q, rolen)
+		bx    = int2octets(sk, rolen)
+	)
+
+	// Step B
+	v := bytes.Repeat([]byte{1}, holen)
+
+	// Step C
+	k := make([]byte, holen)
+
+	// Step D ~ G
+	for i := 0; i < 2; i++ {
+		k = c.Mac(
+			k,
+			v,
+			[]byte{byte(i)},
+			bx,
+			bh,
+		)
+		v = c.Mac(k, v)
+	}
+
+	// Step H
+	for {
+		// Step H1
+		var t []byte
+
+		// Step H2
+		for len(t)*8 < qlen {
+			v = c.Mac(k, v)
+			t = append(t, v...)
+		}
+
+		// Step H3
+		secret := bits2int(t, qlen)
+		if secret.Sign() > 0 && secret.Cmp(q) < 0 {
+			return secret
+		}
+		k = c.Mac(k, append(v, 0x00))
+		v = c.Mac(k, v)
+	}
+}
+
+// https://tools.ietf.org/id/draft-irtf-cfrg-vrf-06.html#rfc.section.5.4.3
+func (c *core) HashPoints(points ...struct{ x, y *big.Int }) *big.Int {
+	v := []byte{c.SuiteString, 0x2}
+	for _, pt := range points {
+		v = append(v, c.Marshal(pt.x, pt.y)...)
+	}
+
+	hash := c.Hash(v)
+	return bits2int(hash, c.OneN())
+}
+
+func (c *core) GammaToHash(x, y *big.Int) []byte {
+	cx, cy := c.ScalarMult(x, y, []byte{c.Cofactor})
+
+	gamma := c.Marshal(cx, cy)
+	return c.Hash(
+		[]byte{c.SuiteString, 0x03},
+		gamma,
+	)
+}
+
+// https://tools.ietf.org/html/rfc6979#section-2.3.2
+func bits2int(in []byte, qlen int) *big.Int {
+	out := new(big.Int).SetBytes(in)
+	if ilen := len(in) * 8; ilen > qlen {
+		return out.Rsh(out, uint(ilen-qlen))
+	}
+	return out
+}
+
+// https://tools.ietf.org/html/rfc6979#section-2.3.3
+func int2octets(v *big.Int, rolen int) []byte {
+	var (
+		out    = v.Bytes()
+		outlen = len(out)
+	)
+
+	// left pad with zeros if it's too short
+	if rolen > outlen {
+		out2 := make([]byte, rolen)
+		copy(out2[rolen-outlen:], out)
+		return out2
+	}
+
+	// drop most significant bytes if it's too long
+	return out[outlen-rolen:]
+}
+
+// https://tools.ietf.org/html/rfc6979#section-2.3.4
+func bits2octets(in []byte, q *big.Int, rolen int) []byte {
+	z1 := bits2int(in, q.BitLen())
+	z2 := new(big.Int).Sub(z1, q)
+	if z2.Sign() < 0 {
+		return int2octets(z1, rolen)
+	}
+	return int2octets(z2, rolen)
+}
+
+func defaultSqrt(c elliptic.Curve, s *big.Int) *big.Int {
+	var r big.Int
+	if nil == r.ModSqrt(s, c.Params().P) {
+		return nil // x is not a square
+	}
+	return &r
+}
--- vrf.go
+++ vrf.go
@@ -12,59 +12,61 @@ type VRF interface {
 	Verify(pk *ecdsa.PublicKey, m []byte, proof []byte) (hash []byte, err error)
 }
 
-func NewSecp256k1Sha256Tai() VRF {
-	cfg := func(curve elliptic.Curve) *config {
-		return &config{
-			curve,
-			0xfe,
-			1,
-			sha256.New,
-			func(x *big.Int) *big.Int {
-				// y² = x³ + b
-				x3 := new(big.Int).Mul(x, x)
-				x3.Mul(x3, x)
+func New(cfg *Config) VRF {
+	return &vrf{func(c elliptic.Curve) *core {
+		return &core{cfg, c}
+	}}
+}
 
-				x3.Add(x3, curve.Params().B)
-				x3.Mod(x3, curve.Params().P)
-				return x3
-			},
-			defaultSqrt,
-		}
-	}
-	return &vrf{cfg}
+func NewSecp256k1Sha256Tai() VRF {
+	return New(&Config{
+		0xfe,
+		1,
+		sha256.New,
+		func(c elliptic.Curve, x *big.Int) *big.Int {
+			// y² = x³ + b
+			x3 := new(big.Int).Mul(x, x)
+			x3.Mul(x3, x)
+
+			x3.Add(x3, c.Params().B)
+			x3.Mod(x3, c.Params().P)
+			return x3
+		},
+		defaultSqrt,
+	})
 }
 
 type vrf struct {
-	cfg func(curve elliptic.Curve) *config
+	newCore func(c elliptic.Curve) *core
 }
 
 func (v *vrf) Prove(sk *ecdsa.PrivateKey, m []byte) (hash []byte, proof []byte, err error) {
 	var (
-		cfg  = v.cfg(sk.Curve)
-		q    = cfg.Q()
+		core = v.newCore(sk.Curve)
+		q    = core.Q()
 		qlen = q.BitLen()
 	)
 
 	// step 1: Hash to curve
-	hx, hy, err := cfg.HashToCurveTryAndIncrement(sk.X, sk.Y, m)
+	hx, hy, err := core.HashToCurveTryAndIncrement(sk.X, sk.Y, m)
 	if err != nil {
 		return
 	}
 
 	// step 2: point to string
-	hbytes := cfg.Marshal(hx, hy)
+	hbytes := core.Marshal(hx, hy)
 
 	// step 3: gamma = x * H
-	gammaX, gammaY := cfg.ScalarMult(hx, hy, sk.D.Bytes())
+	gammaX, gammaY := core.ScalarMult(hx, hy, sk.D.Bytes())
 
 	// step 4: nonce
-	k := cfg.GenerateNonce(sk.D, cfg.Hash(hbytes))
+	k := core.GenerateNonce(sk.D, core.Hash(hbytes))
 	kbytes := k.Bytes()
 
 	// step 5: c = hash points
-	ux, uy := cfg.ScalarBaseMult(kbytes)
-	vx, vy := cfg.ScalarMult(hx, hy, kbytes)
-	c := cfg.HashPoints([]struct{ x, y *big.Int }{
+	ux, uy := core.ScalarBaseMult(kbytes)
+	vx, vy := core.ScalarMult(hx, hy, kbytes)
+	c := core.HashPoints([]struct{ x, y *big.Int }{
 		{hx, hy},
 		{gammaX, gammaY},
 		{ux, uy},
@@ -78,13 +80,13 @@ func (v *vrf) Prove(sk *ecdsa.PrivateKey, m []byte) (hash []byte, proof []byte, 
 	s.Mod(s, q)
 
 	// step 7: encode (gamma, c, s)
-	gammaBytes := cfg.Marshal(gammaX, gammaY)
+	gammaBytes := core.Marshal(gammaX, gammaY)
 
-	cbytes := int2octets(c, (cfg.OneN()+7)/8)
+	cbytes := int2octets(c, (core.OneN()+7)/8)
 	sbytes := int2octets(s, (qlen+7)/8)
 
 	proof = append(append(gammaBytes, cbytes...), sbytes...)
-	hash = cfg.GammaToHash(gammaX, gammaY)
+	hash = core.GammaToHash(gammaX, gammaY)
 	return
 }
 
